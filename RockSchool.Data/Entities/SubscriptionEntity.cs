d46d197ac000b56343a56ca32fef48ac1a2c6825
Ivan Popov <ivan_popov@epam.com>
2025-03-06 09:58:51 +0500

    Migration for Subscriptions is added

--- RockSchool.Data/Entities/SubscriptionEntity.cs
+++ RockSchool.Data/Entities/SubscriptionEntity.cs
@@ -11,28 +11,34 @@ public class SubscriptionEntity
     public bool IsGroup { get; set; }
 
     public Guid StudentId { get; set; }
+
     [ForeignKey(nameof(StudentId))]
     public virtual StudentEntity Student { get; set; }
 
-    public int AttendanceCount { get; set; }
+    public int? AttendanceCount { get; set; }
 
-    public int AttendanceLength { get; set; }
+    public int? AttendanceLength { get; set; }
 
     public DateTime StartDate { get; set; }
 
-    public string Status { get; set; }
+    public int Status { get; set; }
 
     public int DisciplineId { get; set; }
+
     [ForeignKey(nameof(DisciplineId))]
     public virtual DisciplineEntity Discipline { get; set; }
 
     public int? TransactionId { get; set; }
 
     public Guid TeacherId { get; set; }
+
     [ForeignKey(nameof(TeacherId))]
     public virtual TeacherEntity Teacher { get; set; }
+
     public int BranchId { get; set; }
+
     [ForeignKey(nameof(BranchId))]
     public virtual BranchEntity Branch { get; set; }
-    public bool IsTrial { get; set; }
+
+    public int? TrialStatus { get; set; }
 }
---
+++ RockSchool.Data/Migrations/20250306044006_Update_Subscriptions.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RockSchool.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Subscriptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsTrial",
+                table: "Subscriptions");
+
+            migrationBuilder.Sql("ALTER TABLE public.\"Subscriptions\" ALTER COLUMN \"Status\" TYPE integer USING \"Status\"::integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AttendanceLength",
+                table: "Subscriptions",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AttendanceCount",
+                table: "Subscriptions",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TrialStatus",
+                table: "Subscriptions",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrialStatus",
+                table: "Subscriptions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Subscriptions",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AttendanceLength",
+                table: "Subscriptions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AttendanceCount",
+                table: "Subscriptions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTrial",
+                table: "Subscriptions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
