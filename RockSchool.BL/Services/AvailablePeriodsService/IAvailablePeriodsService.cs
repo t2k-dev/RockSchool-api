87efee7e23b278613e3b1e1dd679906d13b25e90
Timothy Zvyagin <timak8000@icloud.com>
2025-03-07 22:26:46 +0500

    Add available periods generation to TeacherController

---
+++ RockSchool.BL/Services/AvailablePeriodsService/IAvailablePeriodsService.cs
@@ -0,0 +1,8 @@
+﻿using RockSchool.BL.Dtos;
+
+namespace RockSchool.BL.Services.AvailablePeriodsService;
+
+public interface IAvailablePeriodsService
+{
+    Task<AvailablePeriodsDto?> GetAvailablePeriodsAsync(int disciplineId, int branchId);
+}
